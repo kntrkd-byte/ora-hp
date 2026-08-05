5f01633ceee721c638da6f90621b6d832336e780
kntrkd-byte <kntr.kd@gmail.com>
2026-08-05 19:04:55 +0900

    feat: add cast/gallery images, business hours 19:00-05:00

--- js/cast-render.js
+++ js/cast-render.js
@@ -1,11 +1,11 @@
 // cast.json の内容をここに直接埋め込み（file://でも動作する）
 const CAST_DATA = [
-  { id:1, name:"あかね", catch:"笑顔と話術で場を彩ります", img:"" },
-  { id:2, name:"みこ",   catch:"上質な時間をともに",        img:"" },
-  { id:3, name:"れな",   catch:"あなたの夜をもっと輝かせたい", img:"" },
-  { id:4, name:"さくら", catch:"洗練された会話で特別な一夜を", img:"" },
-  { id:5, name:"ゆい",   catch:"いつも笑顔でお待ちしています", img:"" },
-  { id:6, name:"なな",   catch:"あなただけの特別な時間を",  img:"" },
+  { id:1, name:"あかね", catch:"笑顔と話術で場を彩ります", img:"assets/images/cast/cast-01.jpg" },
+  { id:2, name:"みこ",   catch:"上質な時間をともに",        img:"assets/images/cast/cast-02.jpg" },
+  { id:3, name:"れな",   catch:"あなたの夜をもっと輝かせたい", img:"assets/images/cast/cast-03.jpg" },
+  { id:4, name:"さくら", catch:"洗練された会話で特別な一夜を", img:"assets/images/cast/cast-04.jpg" },
+  { id:5, name:"ゆい",   catch:"いつも笑顔でお待ちしています", img:"assets/images/cast/cast-05.jpg" },
+  { id:6, name:"なな",   catch:"あなただけの特別な時間を",  img:"assets/images/cast/cast-06.jpg" },
 ];
 
 async function fetchCast() {
